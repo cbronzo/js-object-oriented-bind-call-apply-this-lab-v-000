4f599afe7547104a37d9f32a0514b57bc4e23cfe
Catherine Bronzo <cbronzo@mac.com>
2019-03-18 04:29:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,5 +13,8 @@ function setThisWithApply(fn, thisValue, args) {
 }
 
 function returnNewFunctionOf(functionToBeCopied, thisValue) {
+  const copy = functionToBeCopied.bind(thisValue);
+  return copy;
+}
   
 }
